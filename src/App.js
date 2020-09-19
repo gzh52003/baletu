96169940c55e10e263c2f3c904b94c1f31fac761
wuXueFa <466021779@qq,com>
2020-09-19 16:58:10 +0800

    “环境搭建

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Suspense, lazy} from 'react';
 import './App.css';
+import {withRouter,BrowserRouter,Route,Redirect,Switch,Link,NAVLink} from 'react-router-dom';
+// @withRouter
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+import Home from './views/Home'
+
+const Reg = lazy(() => import("./views/Reg"))
+const Login = lazy(() => import("./views/Login"))
+const List = lazy(() => import("./views/List"))
+const Mine = lazy(() => import("./views/Mine"))
+const Single = lazy(() => import("./views/Single"))
+
+
+
+import {Menu,Row,Col,Button} from 'antd'
+class App extends React.Component{
+  state = {
+  }
+   render(){
+    return (
+    <Suspense fallback={<div>loading...</div>}>
+      <Switch>
+        <Route path="/home" component={Home} />  
+        <Route path="/reg" component={Reg} />  
+        <Route path="/login" component={Login} />  
+        <Route path="/mine" component={Mine} />  
+        <Route path="/list" component={List} />  
+        <Route path="/single" component={Single} /> 
+        <Route path="/notfound" render={() => <div>404</div>} />
+        <Redirect from="/" to="/home" exact />
+      </Switch>
+    </Suspense>
   );
 }
-
+}
 export default App;
