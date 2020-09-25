035f69832fb06adde8aaecc0f5b84254a1330267
yilun95 <852446787@qq.com>
2020-09-25 11:48:16 +0800

    保存版本

--- src/views/List.js
+++ src/views/List.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavBar, ListView , Tag, Tabs, Drawer, icon ,SearchBar, WingBlank, WhiteSpace } from 'antd-mobile';
+import request from '../utils/request'
 
 import 'antd-mobile/dist/antd-mobile.css';
 import './List.scss'
@@ -9,22 +10,22 @@ const tabs = [
     { title: '整租 ∨' },
     { title: '更多 ∨' },
   ];
-const data = [
-    {
-      img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
-      title: 'Meet hotel',
-      des: '不是所有的兼职汪都需要风吹日晒',
-    },
-    {
-      img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
-      title: 'McDonald\'s invites you',
-      des: '不是所有的兼职汪都需要风吹日晒',
-    },
-    {
-      img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
-      title: 'Eat the week',
-      des: '不是所有的兼职汪都需要风吹日晒',
-    },
+const Data = [
+    // {
+    //   img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
+    //   title: 'Meet hotel',
+    //   des: '不是所有的兼职汪都需要风吹日晒',
+    // },
+    // {
+    //   img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
+    //   title: 'McDonald\'s invites you',
+    //   des: '不是所有的兼职汪都需要风吹日晒',
+    // },
+    // {
+    //   img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
+    //   title: 'Eat the week',
+    //   des: '不是所有的兼职汪都需要风吹日晒',
+    // },
   ];
   const NUM_ROWS = 20;
   let pageIndex = 0;
@@ -49,11 +50,11 @@ class List extends React.Component{
           isLoading: true,
         };
       }
-      componentDidMount() {
-        // you can scroll to the specified position
-        // setTimeout(() => this.lv.scrollTo(0, 120), 800);
-    
-        // simulate initial Ajax
+      async componentDidMount() {
+          const {data} = await request.get('list')
+          this.Data = [...data]
+          console.log("Data=",this.Data)
+
         setTimeout(() => {
           this.rData = genData();
           this.setState({
@@ -79,77 +80,65 @@ class List extends React.Component{
         }, 1000);
       }
     render(){
-        const separator = (sectionID, rowID) => (
-            <div
-              key={`${sectionID}-${rowID}`}
-              style={{
-                backgroundColor: '#F5F5F9',
-                height: 8,
-                borderTop: '1px solid #ECECED',
-                borderBottom: '1px solid #ECECED',
-              }}
-            />
-          );
-          let index = data.length - 1;
-            const row = (rowData, sectionID, rowID) => {
-                if (index < 0) {
-                index = data.length - 1;
-                }
-                const obj = data[index--];
-                return (
-                    <div key={rowID} style={{ padding: '0 15px' }}>
-                        <div style={{ display: '-webkit-box', display: 'flex', padding: '15px 0' }}>
-                            <img style={{ height: '64px', marginRight: '15px' }} src={obj.img} alt="" />
-                            <div style={{ lineHeight: 1 }}>
-                            <div style={{ marginBottom: '8px', fontWeight: 'bold' }}>{obj.des}</div>
-                            <div><span style={{ fontSize: '30px', color: '#FF6E27' }}>{rowID}</span>¥</div>
-                            </div>
-                        </div>
-                        </div>
-                );
-            };
+        let index = Data.length - 1;
+        const row = (rowData, sectionID, rowID) => {
+            if (index < 0) {
+            index = Data.length - 1;
+            }
+            const obj = Data[index--];
             return (
+                <div key={rowID} style={{ padding: '0 15px' }}>
+                    <div style={{ display: '-webkit-box', display: 'flex', padding: '15px 0' }}>
+                        <img style={{ height: '64px', marginRight: '15px' }} src={obj.img} alt="" />
+                        <div style={{ lineHeight: 1 }}>
+                        <div style={{ marginBottom: '8px', fontWeight: 'bold' }}>{obj.des}</div>
+                        <div><span style={{ fontSize: '30px', color: '#FF6E27' }}>{rowID}</span>¥</div>
+                        </div>
+                    </div>
+                </div>
+            );
+        };
+        return (
+            <div>
+                <NavBar
+                    mode="light"
+                    leftContent={<i className={`iconfont icon-shouye`} style={{color:'#fff',fontWeight:"bold",fontSize:20}}></i>}
+                    rightContent={<i className={`iconfont icon-wode`} style={{color:'#fff',fontSize:22}}></i>}
+                    style={{backgroundColor:'#ee3943'}}
+                >
+                <div className={'place'}>
+                    <p>上海</p>
+                    <i className={'iconfont icon-jiantou'}></i>
+                </div>
                 <div>
-                    <NavBar
-                        mode="light"
-                        leftContent={<i className={`iconfont icon-shouye`} style={{color:'#fff',fontWeight:"bold",fontSize:20}}></i>}
-                        rightContent={<i className={`iconfont icon-wode`} style={{color:'#fff',fontSize:22}}></i>}
-                        style={{backgroundColor:'#ee3943'}}
-                    >
-                    <div className={'place'}>
-                        <p>上海</p>
-                        <i className={'iconfont icon-jiantou'}></i>
-                    </div>
-                    <div>
-                        <input type="text" placeholder="输入区域，小区搜索房源" style={{borderRadius:5,border:'none',paddingLeft:10,fontSize:12,height:28,width:205}}/>
-                    </div>
-                    </NavBar>
-                    <Tabs tabs={tabs} initialPage={2} animated={false} useOnPan={false} tabBarUnderlineStyle={{display:'none'}} tabBarInactiveTextColor='#5a5c5d'
-                    tabBarActiveTextColor='#ee3943'
-                    tabBarTextStyle={{fontSize:'.26rem'}}
-                    >
-                    </Tabs>
-                    <div className="tag-container">
-                        <Tag disabled>租金月付</Tag>
-                        <Tag disabled>近地铁</Tag>
-                        <Tag disabled>带阳台</Tag>
-                        <Tag disabled>平台认证</Tag>
-                    </div>
-                        
+                    <input type="text" placeholder="输入区域，小区搜索房源" style={{borderRadius:5,border:'none',paddingLeft:10,fontSize:12,height:28,width:205}}/>
+                </div>
+                </NavBar>
+                <Tabs tabs={tabs} initialPage={2} animated={false} useOnPan={false} tabBarUnderlineStyle={{display:'none'}} tabBarInactiveTextColor='#5a5c5d'
+                tabBarActiveTextColor='#ee3943'
+                tabBarTextStyle={{fontSize:'.26rem'}}
+                >
+                </Tabs>
+                <div className="tag-container">
+                    <Tag disabled>租金月付</Tag>
+                    <Tag disabled>近地铁</Tag>
+                    <Tag disabled>带阳台</Tag>
+                    <Tag disabled>平台认证</Tag>
+                </div>
+                    
                 <ListView
-                  ref={el => this.lv = el}
-                  dataSource={this.state.dataSource}
-                  renderRow={row}
-                  className="am-list"
-                  pageSize={4}
-                  useBodyScroll
-                  onScroll={() => { console.log('scroll'); }}
-                  scrollRenderAheadDistance={500}
-                  onEndReached={this.onEndReached}
-                  onEndReachedThreshold={10} 
+                    ref={el => this.lv = el}
+                    dataSource={this.state.dataSource}
+                    renderRow={row}
+                    className="am-list"
+                    pageSize={4}
+                    useBodyScroll
+                    scrollRenderAheadDistance={500}
+                    onEndReached={this.onEndReached}
+                    onEndReachedThreshold={10} 
                 />
             </div>
-            );
+        );
     }
 }
 
